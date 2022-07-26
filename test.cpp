3643a526302c1aedf28476b11fde479f1bf0deed
happydaycoollol <96025782+happydaycoollol@users.noreply.github.com>
2022-07-26 13:02:36 -0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,48 @@
+ #include <iostream>
+using namespace std; 
+
+int main() {
+	cout << "Miku, Miku, you can call me Miku"<< endl;
+    cout << "Blue hair, blue tie, hiding in your wi-fi"<< endl;
+	cout << "Open secrets, anyone can find me"<< endl;
+    cout << "Hear your music running through my mind"<< endl;
+    cout << "We pray for Your mighty hand to ease our suffering"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm on top of the world because of you"<< endl;
+    cout << "All I wanted to do is follow you"<< endl;
+    cout << "All I wanted to do is follow you"<< endl;
+    cout << "I'll keep singing along to all of you"<< endl;
+    cout << "I'll keep singing along"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)"<< endl;
+    cout << "Miku, Miku, what's it like to be you?"<< endl;
+    cout << "20, 20, looking in the rear view"<< endl;
+    cout << "Play me, break me, make me feel like Superman"<< endl;
+    cout << "You can do anything you want"<< endl;
+    cout << "I'm on top of the world because of you"<< endl;
+    cout << "All I wanted to do is follow you"<< endl;
+    cout << "I'll keep singing along to all of you" << endl;
+    cout << "I'll keep singing along" << endl;
+    cout << "I'm on top of the world because of you" << endl;
+    cout << "I do nothing that they could never do" << endl;
+    cout << "I'll keep playing along with all of you" << endl;
+    cout << "I'll keep playing along" << endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)" << endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)" << endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)" << endl;
+    cout << "I'm thinking Miku, Miku (hoo-hee-hoo)" << endl;
+    cout << "Where we were walking together? I will see you in the end" << endl;
+    cout << "I'll take you where you've never been, then bring you back again"<< endl;
+    cout << "Listen to me with your eyes, I'm watching you from in the sky"<< endl;
+    cout << "If you forget I'll fade away, I'm asking you to let me stay"<< endl;
+    cout << "So bathe me in your magic light, and keep it on in darkest night"<< endl;
+    cout << "I need you here to keep me strong, to live my life and sing along"<< endl;
+    cout << "I'm lying with you wide awake, like your expensive poison snake"<< endl;
+    cout << "You found me here inside a dream, walk through the fire straight to me"<< endl;
+    return 0;
+}
